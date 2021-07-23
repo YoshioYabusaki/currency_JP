1538973dd3040d69b230f5ed904c1ad056e1baa6
Yoshio Yabusaki <yoshio@yy.localdomain>
2021-07-23 21:56:08 +0300

    contents of lesson on July 21

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,8 +1,9 @@
 from currency.models import ContactUs, GoodCafe, Rate
+from currency.forms import RateForm
 from currency.utils import generate_password as gen_pass
 
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -21,6 +22,60 @@ def rate_list(request):
         'rate_list': rates,
     }
     return render(request, 'rate_list.html', context=context)
+
+
+def rate_create(request): # スタンダードなフォームのテキスト
+    if request.method == 'POST':
+        form = RateForm(request.POST) # 記入されたらフォーム内容を表示
+        if form.is_valid():
+            form.save() #　フォーム内容を保存
+            return HttpResponseRedirect('/rate/list/')
+    elif request.method == 'GET':
+        form = RateForm() # 最初は空白のフォーム
+    context = {
+        'form': form,
+    }
+    # breakpoint()
+    return render(request, 'rate_create.html', context=context)
+
+
+def rate_details(request, rate_id):
+    rate = get_object_or_404(Rate, id=rate_id)
+    context = {
+        'object': rate,
+    }
+    return render(request, 'rate_details.html', context=context)
+
+
+def rate_update(request, rate_id):
+    rate = get_object_or_404(Rate, id=rate_id)
+
+    if request.method == 'POST':
+        form = RateForm(request.POST, instance=rate) # 元からinstanceの値rateを表示
+        if form.is_valid():
+            form.save() #　フォーム内容を保存
+            return HttpResponseRedirect('/rate/list/')
+    elif request.method == 'GET':
+        form = RateForm(instance=rate) # 元からinstanceの値rateを表示
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'rate_update.html', context=context)
+
+
+def rate_delete(request, rate_id):
+    rate = get_object_or_404(Rate, id=rate_id)
+
+    if request.method == 'POST':
+        rate.delete()
+        return HttpResponseRedirect('/rate/list/')
+
+    # if request.method == 'GET' #ここでは敢えて書く必要がない
+    context = {
+        'object': rate,
+    }
+    return render(request, 'rate_delete.html', context=context)
 
 
 def contact_us_list(request):
