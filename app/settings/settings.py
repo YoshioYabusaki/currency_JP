b0cfdfc71909dc4ea164d19c4eb9f4943f106206
Yoshio Yabusaki <yoshio@yy.localdomain>
2021-11-24 15:21:13 +0200

    add docker-compose

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-7ist2%6a!h%0pio9+cxl-7n_+qwerty4+##ojsts(4!g3y**ex
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # TODO move to env
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -96,13 +96,24 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'currency',
+        'USER': 'db-user',
+        'PASSWORD': 'postgres-password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -256,7 +267,3 @@ SIMPLE_JWT = {
 # STATIC_URL =
 # AWS_DEFAULT_ACL = 'public-read'
 
-try:
-    from settings.settings_local import *
-except ImportError:
-    print('No local settings\n' * 5)
